b5a380cf5ed69103c96543ee8345d31f4af8b700
derekdursteler <derekdursteler@gmail.com>
2019-01-23 16:30:52 -0700

    Ugly colors

--- Assgn1/app.js
+++ Assgn1/app.js
@@ -4,19 +4,9 @@ addTodo.onclick = function () {
     var todoList = document.querySelector("#todo-list");
     var todoName = document.querySelector("#todo-name");
     var newListItem = document.createElement("li");
-    newListItem.setAttribute('id', todoName.value);
     newListItem.innerHTML = todoName.value;
     todoList.appendChild(newListItem);
 };
-
-//var removeTodo = document.querySelector("#remove-todo");
-
-//removeTodo.onclick = function() {
-  //  var todoList = document.querySelector("#todo-list");
-  //  var todoName = document.querySelector("#todo-name");
-  //  var item = document.getElementById(todoName.value);
-   // todoList.removeChild(item);
-//}
 
 fetch("https://api.myjson.com/bins/1gbmos").then(function (response) {
     response.json().then(function (data) {
@@ -26,11 +16,18 @@ fetch("https://api.myjson.com/bins/1gbmos").then(function (response) {
             var todoList = document.querySelector("#todo-list");
             var random = Math.floor(Math.random() * data.length); 
             var newListItem = document.createElement("li");
-            newListItem.setAttribute('id', data[random]);
             newListItem.innerHTML = data[random];
             todoList.appendChild(newListItem);
         };
     });
 });
 
+var removeTodo = document.querySelector("#remove-todo");
+
+removeTodo.onclick = function() {
+    var todoList = document.querySelector("#todo-list");
+    var item = document.querySelector("li");
+    console.log(item)
+    todoList.removeChild(item);
+}
 
