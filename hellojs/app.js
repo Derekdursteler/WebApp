c99c0a4bdf28bc637915b9984e908d52de47e35e
derekdursteler <derekdursteler@gmail.com>
2019-02-08 14:51:29 -0700

    Class server and restaurant example

--- hellojs/app.js
+++ hellojs/app.js
@@ -1,21 +1,41 @@
-// let's change the heading when the page loads
-var heading = document.querySelector("h1");
-console.log("the heading is", heading);
-heading.innerHTML = "Why hello there!";
+var lunchPlaces = null;
 
 // let's make an event
 var theButton = document.querySelector("#the-button");
 console.log("the button is", theButton);
 theButton.onclick = function () {
-  // change the heading again when the button is clicked
-  heading.innerHTML = "You clicked me!";
-  heading.style.color = "#00FF00";
-  heading.style.backgroundColor = "red";
-
-  // add an item to the list when the button is clicked
-  var theList = document.querySelector("#the-list");
-  console.log("the list is", theList);
-  var newItem = document.createElement("li");
-  newItem.innerHTML = "This is a new list item.";
-  theList.appendChild(newItem);
+  var value = "Applebees";
+  var data = "name=" + encodeURIComponent(value);
+  fetch("http://localhost:8080/restaurants", {
+    method: 'POST',
+    body: data,
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded"
+    }
+  }).then(function (response) {
+    console.log("restaurant saved.");
+    getRestaurants();
+  });
 };
+
+var getRestaurants = function() {
+fetch("http://localhost:8080/restaurants").then(function (response) {
+  response.json().then(function (data) {
+    // save all of the data into a global variable (to use later)
+    lunchPlaces = data;
+
+    // data is an array of string values
+    var suggestionsList = document.querySelector("#suggestions");
+    suggestionsList.innerHTML = "";
+    // add the restaurants to the suggestions list
+    data.forEach(function (restaurant) { // for restaurant in data
+      var newItem = document.createElement("li");
+      newItem.innerHTML = restaurant;
+      newItem.className = "restaurant";
+      suggestionsList.appendChild(newItem);
+     });
+   });
+ });
+};
+getRestaurants();
+
--- pyserver/server.py
+++ pyserver/server.py
@@ -1,20 +1,50 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+import json
+
+RESTAURANTS = ["Panda Garden", "Red Lobster", "Chili's", "Chef Alfredo's"]
 
 class MyRequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        if self.path == "/oranges":
+        if self.path == "/restaurants":
             self.send_response(200)
+            # all headers go here:
+            self.send_header("Content-type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
-            self.wfile.write(bytes("I like oranges", "utf-8"))
+            # read the file instead of list right here
+            self.wfile.write(bytes(json.dumps(RESTAURANTS), "utf-8"))
         else:
             self.send_response(404)
             self.end_headers()
-    def do_POST(self):
         return
+
+    def do_POST(self):
+        if self.path == "/restaurants":
+            length = self.headers["Content-length"]
+            body = self.rfile.read(int(length)).decode("utf-8")
+            print("the text body:", body)
+            parsed_body = parse_qs(body)
+            print("the parsed body:", parsed_body)
+
+            # save the restaurant!
+            RESTAURANTS.append(parsed_body["name"][0])
+	    # write the restaurant to file here
+            self.send_response(201)
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+        return
+
 
 def run():
     listen = ("127.0.0.1", 8080)
     server = HTTPServer(listen, MyRequestHandler)
+
     print("Listening...")
     server.serve_forever()
 
