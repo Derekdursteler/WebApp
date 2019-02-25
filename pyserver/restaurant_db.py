f1fc7cacb37d4e3a0ea29d3676fe14874916667e
DerekDursteler <derekdursteler@gmail.com>
2019-02-25 13:50:01 -0700

    Database notes

---
+++ pyserver/restaurant_db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+class RestaurantDB:
+
+    def __init__(self):
+        self.connection = sqlite3.connect("restaurant_db.db")
+        self.connection.row_factory = dict_factory
+        self.cursor = self.connection.cursor()
+
+    def __del__(self): # destructor method
+        # disconnect
+        self.connection.close()
+
+    def createRestaurant(self, name, cuisine, hours, rating):
+        sql = "INSERT INTO restaurants (name, cuisine, hours, ratings) VALUES (?, ?, ?, ?)" # Use ? for placeholder so the client can't do sql injections
+        self.cursor.execute(sql, [name, cuisine, hours, rating]) # The list of variables is data-binding and makes it so the sql injection can't happen, the database takes the value literally
+        self.cursor.commit()
+        return
+
+    def getAllRestaurants(self):
+        self.cursor.execute("SELECT * FROM restaurants")
+        return self.cursor.fetchall()
+        # cursor.fetchone()
--- pyserver/server.py
+++ pyserver/server.py
@@ -1,26 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
 import json
+from restaurant_db import RestaurantDB
 
-RESTAURANTS = ["Panda Garden", "Red Lobster", "Chili's", "Chef Alfredo's"]
+# RESTAURANTS = ["Panda Garden", "Red Lobster", "Chili's", "Chef Alfredo's"] 
 
 class MyRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        # restaurant list action
         if self.path == "/restaurants":
             self.send_response(200)
             # all headers go here:
             self.send_header("Content-type", "application/json")
             self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
-            # read the file instead of list right here
-            self.wfile.write(bytes(json.dumps(RESTAURANTS), "utf-8"))
+            # read data from list, variable, or database right here.
+            db = RestaurantDB() # we do this here in case of the event that we would lose connection to the database, instead of doing outside function and always being connected to the database
+            restaurants = db.getAllRestaurants()
+            self.wfile.write(bytes(json.dumps(restaurants), "utf-8"))
         else:
             self.send_response(404)
             self.end_headers()
         return
 
     def do_POST(self):
+        # restaurant create action
         if self.path == "/restaurants":
             length = self.headers["Content-length"]
             body = self.rfile.read(int(length)).decode("utf-8")
@@ -29,8 +34,16 @@ class MyRequestHandler(BaseHTTPRequestHandler):
             print("the parsed body:", parsed_body)
 
             # save the restaurant!
-            RESTAURANTS.append(parsed_body["name"][0])
-	        # write the restaurant to file here
+            
+            # read the data
+            # name = parsed_body["name"][0]
+            # cuisine = parsed_body["cuisine"][0]
+            # hours = parsed_body["hours"][0]
+            # rating = parsed_body["rating"][0]
+
+	        # send these values to the database
+            # call db.createRestaurant() right here and fill in the fields
+
             self.send_response(201)
             self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
